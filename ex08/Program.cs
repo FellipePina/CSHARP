15242a77e7a061edd59a91889c7c41261be48465
FellipePina <pina.fellipe10@gmail.com>
2025-05-05 22:27:35 -0300

    Organização e modificação final dos exercícios

--- ex08/Program.cs
+++ ex08/Program.cs
@@ -6,10 +6,22 @@ using System.Globalization;
             CultureInfo formato = CultureInfo.InvariantCulture;
 
             Console.Write("Informe seu peso(KG):");
-            float peso = float.Parse(Console.ReadLine(),formato);
+            float peso = 0;
+            bool valido = float.TryParse(Console.ReadLine(),formato,out peso);
+
+            while(!valido){
+                Console.Write("Valor inválido, tente novamente:");
+                valido = float.TryParse(Console.ReadLine(),formato,out peso);
+            }
 
             Console.Write("Informe sua altura(m):");
-            float altura= float.Parse(Console.ReadLine(),formato);
+            float altura= 0;
+            valido = float.TryParse(Console.ReadLine(),formato, out altura);
+
+            while(!valido){
+                Console.Write("Valor inválido, tente novamente:");
+                valido = float.TryParse(Console.ReadLine(),formato,out altura);
+            }
 
             float imc = peso / (altura * altura);
 
@@ -19,10 +31,10 @@ using System.Globalization;
             if(imc < 18.5f){
                 Console.WriteLine("Abaixo do peso");
             }
-            else if(imc < 24.9f){
+            else if(imc <= 24.9f){
                 Console.WriteLine("Peso normal");
             }
-            else if(imc < 29.9f){
+            else if(imc <= 29.9f){
                 Console.WriteLine("Sobrepeso");
             }
             else{
--- ex08/obj/Debug/net9.0/ex08.AssemblyInfo.cs
+++ ex08/obj/Debug/net9.0/ex08.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e4655542c54f053e6a848825d9798217d7fe09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5d7481a54415558112e0f603783bfec3336809")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
